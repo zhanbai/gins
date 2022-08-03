9aa9ccc41104958d574c9aeb4f5304c5bf400cc9
zhanbai <bai.zhan@qq.com>
2022-08-03 17:22:38 +0800

    feat:route

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"gins/bootstrap"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    fmt.Println("Hello World!")
+
+	// new 一个 Gin Engine 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
