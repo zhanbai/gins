e641501f3717e43a03c491c55e2e4a8c0686522f
zhanbai <bai.zhan@qq.com>
2022-08-04 14:02:07 +0800

    feat:cmd seed

--- pkg/seed/seeder.go
+++ pkg/seed/seeder.go
@@ -2,7 +2,10 @@
 package seed
 
 import (
-    "gorm.io/gorm"
+	"gins/pkg/console"
+	"gins/pkg/database"
+
+	"gorm.io/gorm"
 )
 
 // 存放所有 Seeder
@@ -17,19 +20,64 @@ type SeederFunc func(*gorm.DB)
 
 // Seeder 对应每一个 database/seeders 目录下的 Seeder 文件
 type Seeder struct {
-    Func SeederFunc
-    Name string
+	Func SeederFunc
+	Name string
 }
 
 // Add 注册到 seeders 数组中
 func Add(name string, fn SeederFunc) {
-    seeders = append(seeders, Seeder{
-        Name: name,
-        Func: fn,
-    })
+	seeders = append(seeders, Seeder{
+		Name: name,
+		Func: fn,
+	})
 }
 
 // SetRunOrder 设置『按顺序执行的 Seeder 数组』
 func SetRunOrder(names []string) {
-    orderedSeederNames = names
+	orderedSeederNames = names
+}
+
+// GetSeeder 通过名称来获取 Seeder 对象
+func GetSeeder(name string) Seeder {
+	for _, sdr := range seeders {
+		if name == sdr.Name {
+			return sdr
+		}
+	}
+	return Seeder{}
+}
+
+// RunAll 运行所有 Seeder
+func RunAll() {
+
+	// 先运行 ordered 的
+	executed := make(map[string]string)
+	for _, name := range orderedSeederNames {
+		sdr := GetSeeder(name)
+		if len(sdr.Name) > 0 {
+			console.Warning("Running Odered Seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+			executed[name] = name
+
+		}
+	}
+
+	// 再运行剩下的
+	for _, sdr := range seeders {
+		// 过滤已运行
+		if _, ok := executed[sdr.Name]; !ok {
+			console.Warning("Running Seeder: " + sdr.Name)
+			sdr.Func(database.DB)
+		}
+	}
+}
+
+// RunSeeder 运行单个 Seeder
+func RunSeeder(name string) {
+	for _, sdr := range seeders {
+		if name == sdr.Name {
+			sdr.Func(database.DB)
+			break
+		}
+	}
 }
