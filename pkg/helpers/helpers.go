e3ef1c6defae4c6639fd89d9a831da12fe6ed056
zhanbai <bai.zhan@qq.com>
2022-08-04 16:19:51 +0800

    feat:verifycode

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -2,7 +2,9 @@
 package helpers
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
 	mathrand "math/rand"
 	"reflect"
 	"time"
@@ -54,6 +56,20 @@ func RandomString(length int) string {
 	b := make([]byte, length)
 	for i := range b {
 		b[i] = letters[mathrand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// RandomNumber 生成长度为 length 随机数字字符串
+func RandomNumber(length int) string {
+	table := [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, length)
+	n, err := io.ReadAtLeast(rand.Reader, b, length)
+	if n != length {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
 	}
 	return string(b)
 }
