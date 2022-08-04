20ec7e339fb5b5c3abf8330a72e1f060518eac12
zhanbai <bai.zhan@qq.com>
2022-08-04 11:39:59 +0800

    feat:limit

--- routes/api.go
+++ routes/api.go
@@ -2,23 +2,30 @@
 package routes
 
 import (
-    "net/http"
+	"gins/app/http/middlewares"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
-    // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-    v1 := r.Group("/v1")
-    {
-        // 注册一个路由
-        v1.GET("/", func(c *gin.Context) {
-            // 以 JSON 格式响应
-            c.JSON(http.StatusOK, gin.H{
-                "Hello": "World!",
-            })
-        })
-    }
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
+
+	{
+		// 注册一个路由
+		v1.GET("/", func(c *gin.Context) {
+			// 以 JSON 格式响应
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "World!",
+			})
+		})
+	}
 }
