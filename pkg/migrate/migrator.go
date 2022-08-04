1b3b351200ae1c61a9328aa324326a13cced5437
zhanbai <bai.zhan@qq.com>
2022-08-04 13:55:44 +0800

    feat:cmd migrate reset and fresh

--- pkg/migrate/migrator.go
+++ pkg/migrate/migrator.go
@@ -124,6 +124,30 @@ func (migrator *Migrator) rollbackMigrations(migrations []Migration) bool {
 	return runed
 }
 
+// Reset 回滚所有迁移
+func (migrator *Migrator) Reset() {
+
+	migrations := []Migration{}
+
+	// 按照倒序读取所有迁移文件
+	migrator.DB.Order("id DESC").Find(&migrations)
+
+	// 回滚所有迁移
+	if !migrator.rollbackMigrations(migrations) {
+		console.Success("[migrations] table is empty, nothing to reset.")
+	}
+}
+
+// Refresh 回滚所有迁移，并运行所有迁移
+func (migrator *Migrator) Refresh() {
+
+	// 回滚所有迁移
+	migrator.Reset()
+
+	// 再次执行所有迁移
+	migrator.Up()
+}
+
 // 获取当前这个批次的值
 func (migrator *Migrator) getBatch() int {
 
