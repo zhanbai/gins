f547d0dc8643f7990b08ab94a632aecffecd4f10
zhanbai <bai.zhan@qq.com>
2022-08-04 13:57:44 +0800

    feat:cmd migrate fresh

--- pkg/migrate/migrator.go
+++ pkg/migrate/migrator.go
@@ -148,6 +148,25 @@ func (migrator *Migrator) Refresh() {
 	migrator.Up()
 }
 
+// Fresh Drop 所有的表并重新运行所有迁移
+func (migrator *Migrator) Fresh() {
+
+	// 获取数据库名称，用以提示
+	dbname := database.CurrentDatabase()
+
+	// 删除所有表
+	err := database.DeleteAllTables()
+	console.ExitIf(err)
+	console.Success("clearup database " + dbname)
+
+	// 重新创建 migrates 表
+	migrator.createMigrationsTable()
+	console.Success("[migrations] table created.")
+
+	// 重新调用 up 命令
+	migrator.Up()
+}
+
 // 获取当前这个批次的值
 func (migrator *Migrator) getBatch() int {
 
