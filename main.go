bb67642b993fef15fc1e905768af0badc048d479
zhanbai <bai.zhan@qq.com>
2022-08-04 13:18:50 +0800

    feat:cmd make

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"gins/app/cmd"
+	"gins/app/cmd/make"
 	"gins/bootstrap"
 	btsConig "gins/config"
 	"gins/pkg/config"
@@ -13,52 +14,53 @@ import (
 )
 
 func init() {
-    // 加载 config 目录下的配置信息
-    btsConig.Initialize()
+	// 加载 config 目录下的配置信息
+	btsConig.Initialize()
 }
 
 func main() {
 
-    // 应用的主入口，默认调用 cmd.CmdServe 命令
-    var rootCmd = &cobra.Command{
-        Use:   config.Get("app.name"),
-        Short: "A simple forum project",
-        Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   config.Get("app.name"),
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
 
-        // rootCmd 的所有子命令都会执行以下代码
-        PersistentPreRun: func(command *cobra.Command, args []string) {
+		// rootCmd 的所有子命令都会执行以下代码
+		PersistentPreRun: func(command *cobra.Command, args []string) {
 
-            // 配置初始化，依赖命令行 --env 参数
-            config.InitConfig(cmd.Env)
+			// 配置初始化，依赖命令行 --env 参数
+			config.InitConfig(cmd.Env)
 
-            // 初始化 Logger
-            bootstrap.SetupLogger()
+			// 初始化 Logger
+			bootstrap.SetupLogger()
 
-            // 初始化数据库
-            bootstrap.SetupDB()
+			// 初始化数据库
+			bootstrap.SetupDB()
 
-            // 初始化 Redis
-            bootstrap.SetupRedis()
+			// 初始化 Redis
+			bootstrap.SetupRedis()
 
-            // 初始化缓存
-        },
-    }
+			// 初始化缓存
+		},
+	}
 
-    // 注册子命令
-    rootCmd.AddCommand(
-        cmd.CmdServe,
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
 		cmd.CmdKey,
 		cmd.CmdPlay,
-    )
+		make.CmdMake,
+	)
 
-    // 配置默认运行 Web 服务
-    cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
 
-    // 注册全局参数，--env
-    cmd.RegisterGlobalFlags(rootCmd)
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
 
-    // 执行主命令
-    if err := rootCmd.Execute(); err != nil {
-        console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
-    }
+	// 执行主命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
+	}
 }
